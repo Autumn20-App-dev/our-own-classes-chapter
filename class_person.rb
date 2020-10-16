d447ce8705d467b160b281c35e38c5a1513a1ac9
kiana-hob <kianah@uchicago.edu>
2020-10-16 19:42:47 +0000

    Complete

--- class_person.rb
+++ class_person.rb
@@ -7,5 +7,21 @@
 #  - An instance method, age, that calculates the number of years between today and birthdate
 
 class Person
-  
+  # Must require date to use the Date class
+  require("date")
+
+  attr_accessor :first_name
+  attr_accessor :last_name
+  attr_accessor :birthdate
+
+  def full_name()
+    return "#{first_name} #{last_name}"
+  end
+  def age()
+    today = Date.today
+    birth = Date.parse(self.birthdate)
+    age = today.year - birth.year
+    return age
+  end
 end
+
